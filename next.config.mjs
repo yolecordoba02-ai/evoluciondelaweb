cde27bf3cfc007876324f243f4aa0b8d6c792c9b
Yole Cordoba <yolecordoba02@gmail.com>
2026-04-15 17:42:28 -0500

    hola mundo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Habilitar TypeScript estricto en build
   typescript: {
     ignoreBuildErrors: false,
